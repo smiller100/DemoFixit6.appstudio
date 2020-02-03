7d182c028a7ded928f399c778e0adb1627f48658
smiller100 <srm08530@creighton.edu>
2020-02-03 16:34:21 -0600

    Update Welcome.js
    T

--- forms/Welcome/Welcome.js
+++ forms/Welcome/Welcome.js
@@ -3,4 +3,5 @@ let names = ["Bob", "Mary", "Sue"];
 btnShowMessage.onclick=function(){
   console.log(`My best friend is ${names[0]}`);
   lblMessage.textContent = `Another friend is ${names[2]}.`
+  alert(`The second friend is named ${names[1]}.`)
 }
